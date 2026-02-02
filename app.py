e6cb3a90ac53844a65f2baa2b10d47bbb22f8fbe
KIMO0909 <157940987+KIMO0909@users.noreply.github.com>
2026-02-02 17:54:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -207,11 +207,12 @@ with st.form("entry_form", clear_on_submit=True):
                     else:
                         status_box.success(f"✅ 記帳成功！ (${amount})")
                         st.snow()#下雪特效
-                    time.sleep(1)
+                    time.sleep(2)
                     st.rerun()
                 
             except Exception as e:
                 st.error(f"❌ 錯誤: {e}")
+
 
 
 
